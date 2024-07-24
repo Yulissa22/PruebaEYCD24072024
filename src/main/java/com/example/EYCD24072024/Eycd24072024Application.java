027f3f3e47a0d15964ceecd2339699513f9025e5
Yulissa22 <ec1413220@gmail.com>
2024-07-24 11:31:43 -0600

    calculadora

---
+++ src/main/java/com/example/EYCD24072024/Eycd24072024Application.java
@@ -0,0 +1,13 @@
+package com.example.EYCD24072024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eycd24072024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eycd24072024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EYCD24072024/Eycd24072024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EYCD24072024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eycd24072024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
